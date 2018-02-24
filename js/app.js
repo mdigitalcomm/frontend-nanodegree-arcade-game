5e15425b51de5db71011c9f61801d4ce91d665c0
mdigitalcomm <mdigitalcomm@gmail.com>
2018-02-24 15:17:01 -0500

    Finished handleInput function for player

--- js/app.js
+++ js/app.js
@@ -28,8 +28,7 @@ class Enemy {
     }
 
     update(dt) {
-        this.x;
-        this.y;
+        return this.x = 60*dt;
     }
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x = 0, this.y = 60);
@@ -37,9 +36,9 @@ class Enemy {
     }
 }
 
-const bugEnemy = new Enemy();
+const enemy = new Enemy();
 const allEnemies = [];
-allEnemies.push(bugEnemy);
+allEnemies.push(enemy);
 
 // Player class
 // This class requires an update(), render() and
@@ -47,23 +46,55 @@ allEnemies.push(bugEnemy);
 
 
 class Player {
-    constructor() {
+    constructor(x=200, y=400) {
     this.sprite = 'images/char-cat-girl.png';
+    this.x = x;
+    this.y = y;
     }
 
-    update(dt) {
-
+    update(dx=0, dy=0) {
+       this.x += dx;
+       this.y += dy;  
     }
     render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x = 100, this.y = 400);
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
-    handleInput() {
-
-    }
+    
+    handleInput(evt) {       
+        switch (evt.keyCode) {
+            case 37:
+                if (this.x !== 0) {
+                    this.update(-100, 0);
+                    } else {
+                    this.update(0,0);
+                    } //left key pressed    
+            break;            
+            case 38:
+                if (this.y !== 0) {
+                    this.update(0, -80); //up key pressed
+                } else {
+                    this.update(0,0);
+                }               
+            break;            
+            case 39:
+                if (this.x < 400) {
+                    this.update(100, 0); //right key pressed
+                } else {
+                    this.update(0,0);
+                }
+            break;            
+            case 40:
+                if (this.y < 400) {
+                    this.update(0, 80); //down key pressed
+                } else {
+                    this.update(0,0);
+                }
+            break;
+        }
+    }            
 }
 
-const catPlayer = new Player();
-
+const player = new Player();
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -73,13 +104,16 @@ const catPlayer = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+// document.addEventListener('keyup', function(e) {
+    // var allowedKeys = {
+    //     37: 'left',
+    //     38: 'up',
+    //     39: 'right',
+    //     40: 'down'
+    // };
+    // player.handleInput(allowedKeys[e.keyCode]);
+// });
 
-    catPlayer.handleInput(allowedKeys[e.keyCode]);
+document.addEventListener('keydown', function(e) {
+    player.handleInput(e);
 });
