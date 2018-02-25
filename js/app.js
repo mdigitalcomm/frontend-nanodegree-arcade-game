fb1cbac0afcc5c98c8badd171ef3dfa691e8182d
mdigitalcomm <mdigitalcomm@gmail.com>
2018-02-25 17:26:57 -0500

    Added enemy class and key functions

--- js/app.js
+++ js/app.js
@@ -8,43 +8,68 @@
 //    this.sprite = 'images/enemy-bug.png';
 //};
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+
 //Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    
 //};
 
-// Draw the enemy on the screen, required method for game
 //Enemy.prototype.render = function() {
 //    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 //};
 
 //Ememy class
 class Enemy {
-    constructor() {
+    constructor(x = -100, y = Math.floor(Math.random()*3+1)*73, speed = Math.floor((Math.random() * 4)+1)*100) {
         this.sprite = 'images/enemy-bug.png';
+        this.x = x;
+        this.speed = speed;
+        this.y = y;
     }
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+// You should multiply any movement by the dt parameter
+// which will ensure the game runs at the same speed for
+// all computers.
 
     update(dt) {
-        return this.x = 60*dt;
+        this.dt = dt;
+        if (this.x < 500 ) {
+            this.x += this.speed * this.dt;
+        } else {
+            this.speed = Math.floor((Math.random() * 4)+1)*100;
+            this.x = -1 * Math.floor((Math.random() * 6)+1)*100;
+            this.y = Math.floor(Math.random()*3+1)*73;
+        }
     }
+
+    pause() {
+        this.speed = 0;
+    }
+    
     render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x = 0, this.y = 60);
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
     }
 }
 
-const enemy = new Enemy();
+
+const enemy0 = new Enemy();
+const enemy1 = new Enemy();
+const enemy2 = new Enemy();
+const enemy3 = new Enemy();
+const enemy4 = new Enemy();
+const enemy5 = new Enemy();
+
 const allEnemies = [];
-allEnemies.push(enemy);
+// Place all enemy objects in an array called allEnemies
+allEnemies.push(enemy0);
+allEnemies.push(enemy1);
+allEnemies.push(enemy2);
+allEnemies.push(enemy3);
+allEnemies.push(enemy4);
+allEnemies.push(enemy5);
 
 // Player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
 class Player {
     constructor(x=200, y=400) {
     this.sprite = 'images/char-cat-girl.png';
@@ -52,53 +77,76 @@ class Player {
     this.y = y;
     }
 
-    update(dx=0, dy=0) {
-       this.x += dx;
-       this.y += dy;  
+    update(dx=0, dy=0) { 
+        let conflict = this.checkColitions();
+        if (!conflict) {
+            this.x += dx;
+            this.y += dy;
+        } else {
+            setTimeout(this.reset(), 10000);
+        } 
     }
+    
+    checkColitions () {
+        for (const enemy of allEnemies) {
+            const enemyX = enemy.x, 
+                enemyY = Math.floor(enemy.y / 72),
+                playerX = this.x,
+                playerY = Math.floor(this.y / 80);
+                    
+            if (Math.abs(playerX - enemyX) <= 80 && playerY === enemyY) {
+                return true;
+            }
+        }
+    }
+    
+    reset () {
+        this.x = 200;
+        this.y = 400;
+    }
+    
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
     
     handleInput(evt) {       
-        switch (evt.keyCode) {
-            case 37:
-                if (this.x !== 0) {
-                    this.update(-100, 0);
+        
+            switch (evt.keyCode) {
+                case 37: //left key pressed
+                    if (this.x !== 0) {
+                        this.update(-100, 0);
+                        } else {
+                        this.update(0,0);
+                        }  
+                break;            
+                case 38: //up key pressed
+                    if (this.y !== 0) {
+                        this.update(0, -80); 
                     } else {
-                    this.update(0,0);
-                    } //left key pressed    
-            break;            
-            case 38:
-                if (this.y !== 0) {
-                    this.update(0, -80); //up key pressed
-                } else {
-                    this.update(0,0);
-                }               
-            break;            
-            case 39:
-                if (this.x < 400) {
-                    this.update(100, 0); //right key pressed
-                } else {
-                    this.update(0,0);
-                }
-            break;            
-            case 40:
-                if (this.y < 400) {
-                    this.update(0, 80); //down key pressed
-                } else {
-                    this.update(0,0);
-                }
-            break;
+                        this.update(0,0);
+                    }               
+                break;            
+                case 39: //right key pressed
+                    if (this.x < 400) {
+                        this.update(100, 0); 
+                    } else {
+                        this.update(0,0);
+                    }
+                break;            
+                case 40: //down key pressed
+                    if (this.y < 400) {
+                        this.update(0, 80); 
+                    } else {
+                        this.update(0,0);
+                    }
+                break;
+            }
         }
-    }            
+               
 }
 
 const player = new Player();
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
 
 
